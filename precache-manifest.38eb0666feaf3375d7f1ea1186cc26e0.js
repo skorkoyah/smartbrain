3a2ba6fbebfd2e066a9a654e993a10b3ff79003d
skorkoyah <skorkoyah29@gmail.com>
2020-02-12 14:38:33 -0500

    Updates

---
+++ precache-manifest.38eb0666feaf3375d7f1ea1186cc26e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac2a37ac77d0f032c7d209e1ba8d686d",
+    "url": "/smartbrain/index.html"
+  },
+  {
+    "revision": "e24a43044d40d03b6405",
+    "url": "/smartbrain/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "ab14d408c9f2350c5eb0",
+    "url": "/smartbrain/static/css/main.cef589e3.chunk.css"
+  },
+  {
+    "revision": "e24a43044d40d03b6405",
+    "url": "/smartbrain/static/js/2.593c22e3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/smartbrain/static/js/2.593c22e3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ab14d408c9f2350c5eb0",
+    "url": "/smartbrain/static/js/main.a83cdb2e.chunk.js"
+  },
+  {
+    "revision": "2369e4a7bf4bcbe27b68",
+    "url": "/smartbrain/static/js/runtime-main.10e7ac62.js"
+  }
+]);
